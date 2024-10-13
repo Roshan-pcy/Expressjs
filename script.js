0be623b1765f349fcc2308af0e46cf3feaf104a7
Roshan pcy <godxrosu@gmail.com>
2024-10-13 09:44:25 +0530

    added env file and git ignore

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var express=require('express');
 var app=express();
 var port=3000;
-app.use(function(req,res,next){
-    console.log('middle ware running')
-    next();
+const mangoose=require('mongoose');
+mangoose.connect('mongodb+srv://p3@cluster1.q86ki.mongodb.net/notedb').then(function(){
+    app.get('/profile',function(req,res){
+        console.log(req);
+        res.send('Hello profile')
+        
+    });
 })
-app.get('/profile',function(req,res){
-    res.send('Hello profile')
-});
+// app.use(function(req,res,next){
+//     console.log('middle ware running')
+
+//     next();
+// })
+
 
 app.get('/profile/:username',function(req,res){
-    res.send('Hello user ${req.username}')
+    res.send(`Hello user ${req.params.username}`)
 });
-app.listen(port)
+app.listen(port,function(){
+    console.log('server running');
+})
